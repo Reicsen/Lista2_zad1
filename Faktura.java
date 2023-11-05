27b9fa31af974a8d87d8fe6f8e3195d7342f281c
Reicsen <272333@student.pwr.edu.pl>
2023-11-05 20:00:14 +0100

    Stworzenie Faktury

--- Faktura.java
+++ Faktura.java
@@ -1,9 +1,27 @@
 package main.java.com.lista2_zad1;
 
+import java.util.List;
+
+import com.lista2_zad1.Element;
+
 public class Faktura
 {
     public String numerfaktury;
     public String datasprzedazy;
     public String datawystawienia;
+    public Osoba sprzedawca;
+    public Osoba nabywca;
 
+    List<Element> produkty;
+
+    public Faktura(String nr, String sprzedaz, String wystawienie, Osoba sprz, Osoba nab)
+    {
+        this.numerfaktury = nr;
+        this.datasprzedazy = sprzedaz;
+        this.datawystawienia = wystawienie;
+        this.sprzedawca = sprz;
+        this.nabywca = nab;
+
+        this.produkty = new List<>();
+    }
 }
