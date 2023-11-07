96b4d0c7121cbcf8252c4aef2ef6cff492c5c301
Reicsen <272333@student.pwr.edu.pl>
2023-11-07 21:41:24 +0100

    Powiększenie interfejsu

--- DoObslugiBazFaktur.java
+++ DoObslugiBazFaktur.java
@@ -4,4 +4,6 @@ public interface DoObslugiBazFaktur
 {
     String wezInformacjeOFakturze(Faktura f);
     String wezProduktyFaktury(Faktura f);
+    String wezNajnowszyProduktFaktury(Faktura f);
+    String wezSumyFaktury(Faktura f);
 }
